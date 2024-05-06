71273943c28ed69cf2b457cbbe27361c8384eab9
Vanisco Tadiha <tvanisco@gmail.com>
2024-05-06 23:43:16 +0200

    addition of pagination

--- app.vue
+++ app.vue
@@ -3,15 +3,18 @@
     <h1 class="text-2xl font-bold mb-4">Liste des offres d'emploi</h1>
     <div v-if="loading" class="text-gray-600">Chargement en cours...</div>
     <div v-else>
-      <div v-for="job in jobs" :key="job.id" class="bg-white shadow-md rounded-lg p-6 mb-6">
+      <div v-for="job in displayedJobs" :key="job.id" class="bg-white shadow-md rounded-lg p-6 mb-6">
         <h2 class="text-2xl font-bold mb-2">{{ job.title }}</h2>
         <p class="text-gray-600 mb-2">Date de création : {{ formatDate(job.createdAt) }}</p>
         <p class="text-gray-600 mb-2">Slug : {{ job.slug }}</p>
         <div class="flex flex-wrap items-center">
-          <p class="mr-4 text-gray-700"><span class="font-bold">Statut :</span> {{ job.status }}</p>
           <p class="mr-4 text-gray-700"><span class="font-bold">Type :</span> {{ job.type }}</p>
           <p class="mr-4 text-gray-700"><span class="font-bold">Email d'application :</span> <a :href="'mailto:' + job.applicationEmail" class="text-blue-500 hover:underline">{{ job.applicationEmail }}</a></p>
         </div>
+      </div>
+      <div class="flex justify-center mt-4">
+        <button @click="prevPage" :disabled="currentPage === 1" class="mr-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Précédent</button>
+        <button @click="nextPage" :disabled="currentPage === totalPages" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Suivant</button>
       </div>
     </div>
   </div>
@@ -24,7 +27,19 @@ export default {
   data() {
     return {
       jobs: [],
-      loading: true
+      loading: true,
+      currentPage: 1,
+      itemsPerPage: 3 // Nombre d'offres par page
+    }
+  },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.jobs.length / this.itemsPerPage);
+    },
+    displayedJobs() {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+      return this.jobs.slice(startIndex, endIndex);
     }
   },
   async mounted() {
@@ -39,6 +54,16 @@ export default {
     }
   },
   methods: {
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
     formatDate(date) {
       return format(new Date(date), 'dd/MM/yyyy HH:mm');
     }
